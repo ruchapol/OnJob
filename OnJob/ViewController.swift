6be9eb0d2857fb706146d66b3ce9ced659401653
ruchapol.sripruetkait <ruchapol.sripruetkait@scb.co.th>
2019-06-25 17:10:55 +0700

    Initial Commit

---
+++ OnJob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnJob
+//
+//  Created by Ruchapol Sripruetkiat on 25/6/2562 BE.
+//  Copyright © 2562 Ruchapol Sripruetkiat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
